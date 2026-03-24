196d936a2fcf278bcdb872c718c2dad7fec6bc01
valentinbliznakov030-cyber <valentinbliznakov030@gmail.com>
2026-03-24 16:03:47 +0200

    Initial commit: Car Dealership project setup

---
+++ src/main/java/com/mix2020/car_dealership_system/CarDealershipSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mix2020.car_dealership_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mix2020/car_dealership_system/CarDealershipSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mix2020.car_dealership_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDealershipSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
