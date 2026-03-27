84e1be9effd6b6edfae9b751382b884a42e54258
valentinbliznakov030-cyber <valentinbliznakov030@gmail.com>
2026-03-27 16:52:07 +0200

    Add User entity and Role enum

---
+++ src/main/java/com/mix2020/car_dealership_system/user/entity/Role.java
@@ -0,0 +1,6 @@
+package com.mix2020.car_dealership_system.user.entity;
+
+public enum Role {
+    ADMIN,
+    EMPLOYEE
+}
---
+++ src/main/java/com/mix2020/car_dealership_system/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.mix2020.car_dealership_system.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
